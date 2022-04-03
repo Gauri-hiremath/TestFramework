d8f370890945104f77999ded4fc1e2fda2db341e
Gauri@123456 <gaurihiremath26@gmail.com>
2022-04-03 02:02:33 -0700

    Logger Added

--- src/test/java/tests/JenkinsTest.java
+++ src/test/java/tests/JenkinsTest.java
@@ -1,12 +1,14 @@
 package tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class JenkinsTest {
-
+    private static final Logger LOG = LoggerFactory.getLogger(JenkinsTest.class);
     @Test
     public void processName(){
         Map<String,String > map = new HashMap<>();
@@ -18,7 +20,8 @@ public class JenkinsTest {
         map.put("1","Thane");
 
         for(Map.Entry<String,String> entry : map.entrySet() ){
-            System.out.println("Sr. No :"+entry.getKey()+" City: "+entry.getValue());
+            //System.out.println("Sr. No :"+entry.getKey()+" City: "+entry.getValue());
+            LOG.info("Sr. No :"+entry.getKey()+" City: "+entry.getValue());
         }
     }
 }
